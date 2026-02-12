ffbd77111f055d222e664ca001650507b35d480d
gauravdwivedi111 <dwivedigaurav646@gmail.com>
2026-02-12 07:52:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function volume_sphere() {
     //Write your code here
-  
+  let radius = document.getElementById("radius").value;
+	 radius = parseFloat(radius);
+
+	if(isNaN(radius) || radius<0){
+		document.getElementById("volume").value = "NaN";
+		return false;
+	}
+	let volume = (4/3) * Math.PI * Math.pow(radius, 3);
+	volume = volume.toFixed(4);
+
+	document.getElementById("volume").value = volume;
+
+	return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
